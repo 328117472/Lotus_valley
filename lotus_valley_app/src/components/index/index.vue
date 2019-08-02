48bc79067494c37531166bedc45f5f9d5a2d6a4c
Azrael <906347377@qq.com>
2019-08-02 17:49:22 +0800

    图片修改

--- lotus_valley_app/src/components/index/index.vue
+++ lotus_valley_app/src/components/index/index.vue
@@ -13,12 +13,10 @@ export default {
   data() {
     return {
       images:[
-        'https://img06.jiuxian.com/bill/2019/0801/12cd1de66ac84f3d817c314eee06d923.jpg',
-        'https://img06.jiuxian.com/bill/2019/0801/c25486e095744350a25fc693a5ef92b4.jpg',
-        'https://img07.jiuxian.com/bill/2019/0801/f8c6d4c7c9ea4001932635d1993440e2.jpg',
-        'https://img09.jiuxian.com/bill/2019/0801/9bae3d570ecf4538b749a2a955a145da.jpg',
-        'https://img07.jiuxian.com/bill/2019/0801/5520a1feb5604833ba4e5d1e61d37f40.jpg',
-      ]
+        '../../imgs/index/c61ea92218d541d894c5c2db4d17a954.jpg',
+        '../../imgs/index/a11f4fb8ea2b431b9bd739795954d616.jpg',
+        '../../imgs/index/4d3346b5a6474c4c823a28d4a8613ec0.jpg',
+        '../../imgs/index/4d3346b5a6474c4c823a28d4a8613ec0.jpg',
     }
   },
 };
