e702f88dc05063b7eb1bd416ad2cc593cc7ae962
Azrael <906347377@qq.com>
2019-08-02 17:02:19 +0800

    更新组件

---
+++ lotus_valley_app/src/components/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <van-swipe :autoplay="3000" id="a1">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images:[
+        'https://img06.jiuxian.com/bill/2019/0801/12cd1de66ac84f3d817c314eee06d923.jpg',
+        'https://img06.jiuxian.com/bill/2019/0801/c25486e095744350a25fc693a5ef92b4.jpg',
+        'https://img07.jiuxian.com/bill/2019/0801/f8c6d4c7c9ea4001932635d1993440e2.jpg',
+        'https://img09.jiuxian.com/bill/2019/0801/9bae3d570ecf4538b749a2a955a145da.jpg',
+        'https://img07.jiuxian.com/bill/2019/0801/5520a1feb5604833ba4e5d1e61d37f40.jpg',
+      ]
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#a1{
+  width: 200px;height: 300px;
+  // background: #f0f
+}
+</style>
--- lotus_valley_app/src/router.js
+++ lotus_valley_app/src/router.js
@@ -1,22 +1,33 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import test from './components/test.vue'
-import user from './components/login/user.vue'
-import login from './components/login/login.vue'
-import reg from './components/login/reg.vue'
+/* jshint esversion:6 */
+import Vue from 'vue';
+import Router from 'vue-router';
+import index from './components/index/index.vue';
+import user from './components/login/user.vue';
+import login from './components/login/login.vue';
+import reg from './components/login/reg.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   base: process.env.BASE_URL,
-  routes: [
-    {path: '/user',component: user,
+  routes: [{
+      path: '/',
+      component: index
+    },
+    {
+      path: '/user',
+      component: user,
       children: [
         // {path: '/',redirect: login},
-        {path: 'login',component: login}
+        {
+          path: 'login',
+          component: login
+        }
       ]
     },
-    {path: '/reg',component: reg}
+    {
+      path: '/reg',
+      component: reg
+    }
   ]
-})
+});
