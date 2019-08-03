05b8cfe9501e0e525f01db44addc1c82664dff4a
Azrael <906347377@qq.com>
2019-08-03 09:38:17 +0800

    轮播图

--- lotus_valley_app/src/components/index/index.vue
+++ lotus_valley_app/src/components/index/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <van-swipe :autoplay="3000" id="a1">
+    <van-swipe :autoplay="3000">
       <van-swipe-item v-for="(image, index) in images" :key="index">
         <img v-lazy="image" />
       </van-swipe-item>
@@ -12,19 +12,32 @@
 export default {
   data() {
     return {
-      images:[
-        '../../imgs/index/c61ea92218d541d894c5c2db4d17a954.jpg',
-        '../../imgs/index/a11f4fb8ea2b431b9bd739795954d616.jpg',
-        '../../imgs/index/4d3346b5a6474c4c823a28d4a8613ec0.jpg',
-        '../../imgs/index/4d3346b5a6474c4c823a28d4a8613ec0.jpg',
+     images: [
+        "https://img08.jiuxian.com/bill/2019/0801/c61ea92218d541d894c5c2db4d17a954.jpg",
+        "https://img09.jiuxian.com/bill/2019/0802/a11f4fb8ea2b431b9bd739795954d616.jpg",
+        "https://img08.jiuxian.com/bill/2019/0801/4d3346b5a6474c4c823a28d4a8613ec0.jpg",
+        "https://img09.jiuxian.com/bill/2019/0801/9bae3d570ecf4538b749a2a955a145da.jpg",
+        "https://img07.jiuxian.com/bill/2019/0802/d37096ce34b84e12882ac2b84ef97dc1.jpg",
+        "https://img10.jiuxian.com/bill/2019/0801/9b9b7199033145018ec93226956043b4.jpg",
+        "https://img10.jiuxian.com/bill/2019/0801/0721a85f2a5a45599e3fe2c3d3727acb.jpg",
+        "https://img08.jiuxian.com/bill/2019/0801/b5bfe5087d2a47158bbfd530d19db9a8.jpg"
+      ]
     }
   },
+  created() {
+    console.log(this.images);
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-#a1{
-  width: 200px;height: 300px;
-  // background: #f0f
+#a1 {
+  width: 400px;
+  height: 400px;
+}
+img {
+  width: 100%;
+  height: 100%;
+  touch-action: none;
 }
 </style>
